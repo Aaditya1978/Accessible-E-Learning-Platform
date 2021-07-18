bf846b6f7e3aaca3d7f7ecd0a83a5c4dfc595f6d
Abhay_Dhiman <abhaysdhimans@gmail.com>
2021-07-18 14:42:39 +0530

    Added a basic response query for signup page

--- e_learning/api/urls.py
+++ e_learning/api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path,include
-from .views import RoomView
+from .views import TeacherSignUpView, StudentSignUpView, UserLogin
 urlpatterns = [
-    path('',RoomView.as_view()),
+    path('teacher_sign_up',TeacherSignUpView.as_view()),
+    path('student_sign_up', StudentSignUpView.as_view()),
+    path('user_login', UserLogin.as_view()),
 ]
--- e_learning/api/views.py
+++ e_learning/api/views.py
@@ -1,8 +1,68 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework import generics
+from rest_framework import generics, status
+from .serializers import TeacherSerializer, StudentSerializer
+from .models import Student, Teacher
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 # Create your views here.
-class RoomView(generics.CreateAPIView):
+
+
+class TeacherSignUpView(APIView):
+    serializer_class = TeacherSerializer
+
     def post(self, request):
-        return HttpResponse("Hello, world. You're at the polls index.")
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            first_name = serializer.data.get("first_name")
+            last_name = serializer.data.get("last_name")
+            email = serializer.data.get("email")
+            org_name = serializer.data.get("org_name")
+            password = serializer.data.get("password")
+        data = Teacher()
+        data.first_name = first_name
+        data.last_name = last_name
+        data.email = email
+        data.org_name = org_name
+        data.password = password
+        data.save()
+        return Response({"Success": "data saved"}, status=status.HTTP_200_OK)
+
+
+class StudentSignUpView(APIView):
+    serializer_class = StudentSerializer
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            first_name = serializer.data.get("first_name")
+            last_name = serializer.data.get("last_name")
+            email = serializer.data.get("email")
+            class_code = serializer.data.get("class_code")
+            password = serializer.data.get("password")
+        data = Student()
+        data.first_name = first_name
+        data.last_name = last_name
+        data.email = email
+        data.class_code = class_code
+        data.password = password
+        data.save()
+        return Response({"Success": "data saved"}, status=status.HTTP_200_OK)
+
+class UserLogin(APIView):
+    def post(self, request):
+        curr_email = request.data.get("email", None)
+        curr_password = request.data.get("password", None)
+        
+        student_obj = Student.objects.filter(email=curr_email, password=curr_password)
+        teacher_obj = Teacher.objects.filter(email=curr_email, password=curr_password)
+        
+        if student_obj:
+            return Response({"Success": "data saved"}, status=status.HTTP_200_OK)
+        elif teacher_obj:
+            return Response({"Success": "data saved"}, status=status.HTTP_200_OK)
+        else:
+            print("Wrong credentials!!")
+            return Response({"Success": "data saved"}, status=status.HTTP_200_OK)
+        
+        # return Response({"Success": "data saved"}, status=status.HTTP_200_OK)
